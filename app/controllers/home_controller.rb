9d1c943a728f89d8d052f8d5a0a4e0ff73916c7a
Mark Rawlingson <mark.rawlingson@gmail.com>
2016-05-09 01:49:50 +0000

    also search through /r/wallpaper and grab a random sample of 25 listings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     def index
         u = Redd.it(:userless, ENV["CLIENT_ID"], ENV["SECRET"])
         u.authorize!
-        @listings = u.get_hot("wallpapers", {:limit => 6})
+        @listings = u.get_hot("wallpapers+wallpaper", {:limit => 25}).sample(10)
     end
     
 end
