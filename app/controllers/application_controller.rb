fc7b3bd4273542da4bfd3a256fd4573481ea7d5e
Mark Rawlingson <mark.rawlingson@gmail.com>
2016-05-09 01:43:12 +0000

    get default subreddits, display at top of page similar to reddit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :default
+  
+  def default
+    u = Redd.it(:userless, ENV["CLIENT_ID"], ENV["SECRET"])
+    u.authorize!
+    @subreddits = u.get("/subreddits/default.json").body[:data][:children]
+  end
 end
