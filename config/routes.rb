38f2013d62fbaaf9b06c2462cd856e665b1c33dd
Mark Rawlingson <mark.rawlingson@gmail.com>
2016-05-12 06:27:30 +0000

    re-route root to subreddits instead of home controller since it does the same thing and makes more sense, add resources for authorize controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  root "home#index"
+  root "subreddits#index"
   
   get '/r/:name', to: 'subreddits#index'
   get 'sessions/new'
 
+  resources :authorize
   resources :preferences
   resources :users
   
