f5157d680e94e4b2f8c17bed57663678477e3461
Mark Rawlingson <mark.rawlingson@gmail.com>
2016-05-12 06:25:06 +0000

    add helper method for redirect uri

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :redirect_uri
   
   before_action :default
-  
   def default
     u = Redd.it(:userless, ENV["CLIENT_ID"], ENV["SECRET"])
     u.authorize!
     @subreddits = u.get("/subreddits/default.json").body[:data][:children]
+  end
+
+  def redirect_uri
+     "#{request.protocol}#{request.host}#{authorize_index_path}"
   end
 end
