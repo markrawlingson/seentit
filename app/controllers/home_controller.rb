ab9805931abb90ad3f7030e0f672fcc40c9a9c9d
Mark Rawlingson <mark.rawlingson@gmail.com>
2016-05-12 06:34:37 +0000

    remove home controller as no longer needed

--- app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-
-    def index
-        u = Redd.it(:userless, ENV["CLIENT_ID"], ENV["SECRET"])
-        u.authorize!
-        @listings = u.get_hot("wallpapers+wallpaper", {:limit => 25}).sample(10)
-    end
-    
-end
